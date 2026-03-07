341c35ae29e917ed0841e3a0646544c3d9b2c463
Luis Quinones <35478263+LuisQBlanco@users.noreply.github.com>
2026-03-07 03:51:00 +0000

    Update 3dforge and deploy action on GitHub

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/3dforce",
-  assetPrefix: "/3dforce",
+  basePath: "/3dforge",
+  assetPrefix: "/3dforge",
 };
 
 module.exports = nextConfig;
